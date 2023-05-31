b2e09f17d1b6ac0309aa71d6fc93e8e28a14633f
Evan Richard <evan@iflabs.network>
2023-05-31 16:31:33 -0400

    Update iron.py
    missed hunk

--- coins/iron.py
+++ coins/iron.py
@@ -6,8 +6,32 @@ MAX_RETRIES = 5  # maximum number of reconnection attempts
 
 async def connect(pool, retries=0):
     host, port = pool.split(':')
+    writer = None
     try:
         reader, writer = await asyncio.open_connection(host, port)
+        writer.write(encode(
+            {"id": 0, "method": "mining.subscribe",
+             "body": {"version": 2, "agent": "Rigel/1.4.1", "name": "wrk",
+                      "publicAddress": "719e1a617a96353049f82c953938d3ca6d9e89f7ea8e88308b7ec5a3aea8cee8"}}))
+        while True:
+            try:
+                data = await reader.readline()
+            except Exception as ex:
+                print(f'{pool}\t{ex}')
+                break
+
+            received_at = round(time.time() * 1000)
+            msg = decode(data)
+            if not msg:
+                break
+
+            method = msg.get('method')
+
+            if method != 'mining.notify':
+                continue
+
+            height = int.from_bytes(bytes.fromhex(msg['body']['header'][16:24]), 'little')
+            asyncio.create_task(save_work('iron', pool, height, received_at, 60))
     except Exception as ex:
         print(f'{pool}\t{ex}')
         if retries < MAX_RETRIES:
@@ -15,30 +39,9 @@ async def connect(pool, retries=0):
             await connect(pool, retries + 1)
         else:
             print(f"Failed to connect to {pool} after {MAX_RETRIES} attempts")
-        return
-    writer.write(encode(
-        {"id": 0, "method": "mining.subscribe",
-         "body": {"version": 2, "agent": "Rigel/1.4.1", "name": "wrk",
-                  "publicAddress": "719e1a617a96353049f82c953938d3ca6d9e89f7ea8e88308b7ec5a3aea8cee8"}}))
-    while True:
-        try:
-            data = await reader.readline()
-        except Exception as ex:
-            print(f'{pool}\t{ex}')
-            break
-
-        received_at = round(time.time() * 1000)
-        msg = decode(data)
-        if not msg:
-            break
-
-        method = msg.get('method')
-
-        if method != 'mining.notify':
-            continue
-
-        height = int.from_bytes(bytes.fromhex(msg['body']['header'][16:24]), 'little')
-        asyncio.create_task(save_work('iron', pool, height, received_at, 60))
-
+    finally:
+        if writer:
+            writer.close()  # ensure to close the writer
+            await writer.wait_closed()  # wait for the writer to close
     print(f'Reconnecting to {pool}')
     await connect(pool)
